03981bc929e94b84dd38d3ad08c89cf69d50e561
antoine <antoine.frisco@gmail.com>
2021-06-23 17:52:00 +0200

    start dev createcard

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
+class photographers(){
+    constructor (){
+        this.name = name;
+        this.id = id;
+        this.city = city;
+        this.country = country;
+        this.tags = tags;
+        this.tagline = tagline;
+        this.price = price;
+        this.portrait = portrait;
+    }
+}
+
 function createPhotographerCard (){
-    for (i)
+    for
 }
