96c8a003accc0317a8e8f60de8862795d3bb1e56
antoine <antoine.frisco@gmail.com>
2021-07-07 18:09:25 +0200

    alts

---
+++ scripts/profiles.js
@@ -0,0 +1,10 @@
+class Media {
+    constructor(){
+        this.title = media.title;
+        this.id = media.id;
+        this.Id = media.Id;
+        this.tags = media.tags;
+        this.likes = media.likes;
+        this.date = media.date;
+        this.price = media.price;
+    }
