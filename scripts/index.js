89f72d57763a240aab93926a77d3e1b20170dcd4
antoine <antoine.frisco@gmail.com>
2021-07-08 08:57:29 +0200

    test

--- scripts/index.js
+++ scripts/index.js
@@ -12,5 +12,17 @@ class photographers(){
 }
 
 function createPhotographerCard (){
-    for
+    for (let i = 0; i < photographers.length; i++){
+    let photographer = document.createElement('a');
+        let  = document.createElement('header');
+        let  = document.createElement('div');
+        let  = document.createElement('section');
+        let  = document.createElement('ul');
+        let  = document.createElement('img');
+        let  = document.createElement('h2');
+        let  = document.createElement('p');
+        let = document.createElement('p');
+        let = document.createElement('p')
+    }
+        
 }
