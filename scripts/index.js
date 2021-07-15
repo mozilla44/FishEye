32a35a4b91e91dd8ec0ade8bfd38ada491c7c3e5
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-07-15 10:04:06 +0200

    Session du 15 juillet 2021

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,9 @@
-class photographers(){
-    constructor (){
-        this.name = name;
-        this.id = id;
-        this.city = city;
-        this.country = country;
-        this.tags = tags;
-        this.tagline = tagline;
-        this.price = price;
-        this.portrait = portrait;
+class Photographer {
+    constructor (data) {
+        this.id = data.id;
+        this.name = data.name
+        this.price = data.price
+        this.toto = data.toto
     }
 }
 
@@ -26,3 +22,57 @@ function createPhotographerCard (){
     }
         
 }
+
+/* Méthode à la papa
+fetch('/data.json')
+    .then(function(res) {
+        return res.json()
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+    .catch(function(err) {
+        console.log(err)
+    })
+*/
+
+fetch('/data.json')
+    .then(res => res.json())
+    .then((data) => {
+        const photographers = data.photographers
+
+        console.log("====")
+        console.log(photographers)
+        console.log("====")
+
+        for (let i = 0; i < photographers.length; i++) {
+            const Photographers = new Photographer(photographers[i])
+
+            console.log(Photographers)
+        }
+
+    })
+    .catch(function(err) {
+        console.log(err)
+    })
+
+
+class Bottle {
+    constructor(color, brand, contenance) {
+        this.color = color
+        this.brand = brand
+        this.contenance = contenance
+    }
+
+    drink() {
+        console.log(`Je bois de l'eau de chez ${this.brand}`)
+    }
+}
+
+
+const BottleOfEvian = new Bottle('blue', 'evian', 100)
+const BottleOfPerrier = new Bottle('white', 'perrier', 200)
+
+
+BottleOfEvian.drink()
+BottleOfPerrier.drink()
