8c507f2b5fb5769452a975fc9ed37d945eb93b6d
antoine <antoine.frisco@gmail.com>
2021-08-06 10:46:41 +0300

    fetch

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,17 @@
 class Photographer {
     constructor (data) {
+        this.name = data.name;
         this.id = data.id;
-        this.name = data.name
-        this.price = data.price
-        this.toto = data.toto
+        this.city = data.city;
+        this.country = data.country;
+        this.tags = data.tags;
+        this.tagline = data.tagline;
+        this.price = data.price;
+        this.portrait = data.portrait;
     }
 }
+
+
 
 function createPhotographerCard (){
     for (let i = 0; i < photographers.length; i++){
@@ -23,4 +29,35 @@ function createPhotographerCard (){
         
 }
 
+    
+fetch('/data.json')
+.then(res => res.json())
+.then((data) => {
+    const photographers = data.photographers
+
+    console.log("====")
+    console.log(photographers)
+    console.log("====")
+
+    for (let i = 0; i < photographers.length; i++) {
+        const Photographers = new Photographer(photographers[i])
+
+        console.log(Photographers)
+    }
+
+})
+.catch(function(err) {
+    console.log(err)
+})
+
+
+fetch('/data.json')
+    .then(function(res) {
+        return res.json()
+    })
+    .then(function(data) {
+        console.log(data)
+    })
     .catch(function(err) {
+        console.log(err)
+    })
