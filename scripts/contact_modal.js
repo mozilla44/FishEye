6f2feb6ce4d6a98f4f7731b8be0d415d8fbce48f
antoine <antoine.frisco@gmail.com>
2021-08-26 08:29:19 +0200

    fix regex

--- scripts/contact_modal.js
+++ scripts/contact_modal.js
@@ -6,9 +6,11 @@
 function validateContact (){
     let regexName = /^\S\D{2,}$/;
     let regexEmail = /^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]­{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$/;
+    let regexMessage = /(.*[a-z]){3}/i;
     let testFirst = regexName.test(document.getElementById("first").value);
     let testLast = regexName.test(document.getElementById("last").value);
     let testEmail = regexEmail.test(document.getElementById("email").value);
+    let testMessage = regexMessage.test(document.getElementById("form_message").value);
 
     if (testFirst == false){
     	document.getElementById("errorFirst").innerHTML = "Veuillez entrer un prénom valide";
@@ -27,7 +29,14 @@ function validateContact (){
     document.getElementById("email").style.border = "2px solid red"
   }
   else {
-    document.getElementById("errorEmail").style.display = "none" , document.getElementById("email").style.border = "2px solid green"
+    document.getElementById("errorEmail").style.display = "none" , document.getElementById("form_message").style.border = "2px solid green"
+  }
+  if (testMessage == false){
+    document.getElementById("errorMessage").innerHTML = "veuillez entrer au moins 2 caractères"
+    document.getElementById("email").style.border = "2px solid red"
+  }
+  else {
+    document.getElementById("errorMessage").style.display = "none" , document.getElementById("form_message").style.border = "2px solid green"
   }
   
 }
