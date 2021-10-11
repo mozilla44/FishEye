450ad6f83634f0ee413efa1a440fb78f1d41a660
antoine <antoine.frisco@gmail.com>
2021-10-11 16:36:06 +0200

    created cards from jd

--- scripts/index.js
+++ scripts/index.js
@@ -10,28 +10,63 @@ fetch('../data.json')
     let photographerData = data.photographers
     photographerData.forEach(photographer => {
 
-        console.log(photographer.name)
+        //create new photographer card
+        let newPhotographer = document.createElement("div");
+        newPhotographer.classList.add("photographer_card");
+        let section = document.getElementById("photographers_section");
+        section.appendChild(newPhotographer);
 
-        //create divs 
-        let newDiv = document.createElement("div");
-        let newdiv2 = document.createElement("div")
+        //create photogrpaher link (clicakble img)
+        let photographerLink = document.createElement("a");
+        photographerLink.classList.add("profile");
+        newPhotographer.appendChild(photographerLink);
 
-        //add class to creted divs
-        newDiv.classList.add("testclass");
-        newdiv2.classList.add("testclas2")
+        //create profile picture
+        let photographerImg = document.createElement("img");
+        photographerImg.classList.add("rounded_img");
+        photographerImg.setAttribute("src", "images/sample_photos/Photographers_ID_Photos/" + photographer.portrait)
+        photographerLink.appendChild(photographerImg);
 
-        //add innerHTML to created divs
-        newDiv.innerHTML = photographer.name;
-        newdiv2.innerHTML = photographer.city;
+        //create photogrpaher info div
+        let photographerInfo = document.createElement("div");
+        photographerInfo.classList.add("photographer_info");
+        newPhotographer.appendChild(photographerInfo);
 
-        //append div1 to main section
-        let section = document.getElementById("photographers_section")
-        section.appendChild(newDiv);
+        // create photographer name
+        let photographerName = document.createElement("h2");
+        photographerName.classList.add("photographer_name")
+        photographerName.innerHTML = photographer.name;
+        photographerInfo.appendChild(photographerName)
 
-        //append div2 to div1 (casse la boucle)
-        let div1 = document.getElementsByClassName(".testclass")
-        div1.appendChild(newdiv2) //casse la boucle  
-        //section.appendChild(newdiv2) // fonctionne mais pas ce que je veux faire 
+        //create photographer location div
+        let photographerLocation = document.createElement("div")
+        photographerLocation.classList.add("photographer_location");
+        photographerInfo.appendChild(photographerLocation);
+
+        //create photographer city
+        let photographerCity = document.createElement("h3")
+        photographerCity.classList.add("photographer_city")
+        photographerCity.innerHTML = photographer.city + "," + "&nbsp";
+        photographerLocation.appendChild(photographerCity);
+
+        //create photographer country
+        let photographerCountry = document.createElement("h3")
+        photographerCountry.classList.add("photographer_country")
+        photographerCountry.innerHTML = photographer.country;
+        photographerLocation.appendChild(photographerCountry)
+
+        //create photographer tagline
+        let photographerTagline = document.createElement("p")
+        photographerTagline.classList.add("photographer_tagline")
+        photographerTagline.innerHTML = photographer.tagline;
+        photographerInfo.appendChild(photographerTagline);
+
+        //create photographer price
+        let photographerPrice = document.createElement("p")
+        photographerPrice.classList.add("photographer_price")
+        photographerPrice.innerHTML = photographer.price + "€/jour";
+        photographerInfo.appendChild(photographerPrice)
+
     });
     })
 .catch(function (err) {
