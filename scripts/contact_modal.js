1cf2c5252d32e867c3d0866e06fbc877e359d45d
antoine <antoine.frisco@gmail.com>
2021-11-03 14:00:04 +0100

    inneHTML v2

--- scripts/contact_modal.js
+++ scripts/contact_modal.js
@@ -8,146 +8,46 @@ const modalForm = (profile) => {
   let bground = document.createElement("section")
   bground.classList.add("bground")
   bground.setAttribute("id" , "close_modal_btn")
+  bground.innerHTML = ` <i class="fas fa-times" id="close_modal_btn"></i>
+  <div class="modal_content">
+      <form name="form" id="contact_form" class="form" action="" onsubmit="return validateContact()">
+          <h1 class="contact_name">Contactez-moi</h1>
+          <h2 class="modal_photographer_name">Mimi Keel</h2>
+          <div class="formData" id="input_first">
+              <p class="input_name">Prénom</p>
+              <label for="first"></label>
+              <input class="text-control" type="text" id="first" name="first" minlength="2">
+              <p id="errorFirst" class="errorMesage"></p>
+              <div class="formData" id="input_last">
+                  <p class="input_name">Nom</p>
+                  <label for="last"></label>
+                  <input class="text-control" type="text" id="last" name="last">
+                  <p id="errorLast" class="errorMesage"></p>
+              </div>
+              <div class="formData" id="input_email">
+                  <p class="input_name">email</p>
+                  <label for="email"></label>
+                  <input class="text-control" type="" id="email" name="email" placeholder="adresse@email.com">
+                  <p id="errorEmail" class="errorMesage"></p>
+              </div>
+              <div class="formData" id="input_message">
+                  <p class="input_name">Votre message</p>
+                  <label for="message"></label>
+                  <input class="text-control" type="text" id="form_message" name="message"
+                      aria-label="Your message"></input>
+                  <p id="errorFormMesage" class="errorMesage"></p>
+              </div>
+              <div id="send_btn">
+                  <input class="btn btn_primary" id="contact-submit-btn" type="submit" role="button" aria-label="Send"
+                      value="Envoyer" onclick="validateContact()">
+              </div>
+          </div>
+      </form>
+  </div>`
   let main = document.getElementById("main");
   main.appendChild(bground)
   
-  let closeIcon  = document.createElement("i")
-  closeIcon.classList.add("fas" , "fa-times")
-  closeIcon.setAttribute("id" , "close_modal_btn")
-  bground.appendChild(closeIcon)
-
-  let modalContent = document.createElement("div")
-  modalContent.classList.add("modal_content");
-  bground.appendChild(modalContent)
-
-  let formBody = document.createElement("form")
-  formBody.setAttribute("name" , "form")
-  formBody.setAttribute("id", "contact_form")
-  formBody.setAttribute("class", "form")
-
-  let contactMe = document.createElement("h1")
-  contactMe.classList.add("contact_name")
-  contactMe.innerHTML = "Contactez-moi"
-  formBody.appendChild(contactMe)
-
-  let profileName = document.createElement("h2")
-  profileName.classList.add("modal_photographer_name")
-  profileName.innerHTML = profile.name
-  formBody.appendChild(profileName)
-
-  //First name field
-
-  let firstName = document.createElement("div")
-  firstName.classList.add("FormData")
-  firstName.setAttribute("id","input_first")
-  formBody.appendChild(firstName)
-
-  let inputFirstName = document.createElement("p")
-  inputFirstName.classList.add("input_name")
-  inputFirstName.innerHTML = "Prénom"
-  firstFirstName.appendChild(inputFirstName)
-
-  let firstLabel = document.createElement("label")
-  firstLabel.setAttribute("for","first")
-  firstName.appendChild(firstLabel)
-
-  let firstInput = document.createElement("input")
-  firstInput.classList.add("text_control")
-  firstInput.setAttribute("type","text")
-  firstInput.setAttribute("id" , "first")
-  firstInput.setAttribute("name" ,"first")
-  firstInput.setAttribute("minlength", "2")
-  firstName.setAttribute(firstInput)
-
-  let errorFirst = document.createElement("p")
-  errorFirst.classList.add("errorMesage")
-  errorFirst.setAttribute("id", "errorFirst")
-
-  //last name field
-
-  let inputLast = document.createElement("div")
-  inputLast.classList.add("formData")
-  inputLast.setAttribute("id" ,"input_last")
-  firstName.appendChild(inputLast)
-
-  let inputLastName = document.createElement("p")
-  inputLastName.classList.add("input_name")
-  inputLastName.innerHTML = "Nom"
-  inputLast.appendChild(inputLastName)
-
-  let lastLabel = document.createElement("label")
-  lastLabel.setAttribute("for" , "last")
-  inputLast.appendChild(lastLabel)
-
-  let lastNameInput = document.createElement("input")
-  lastNameInput.classList.add("text-control")
-  lastNameInput.setAttribute("type", "text")
-  lastNameInput.setAttribute("id", "last")
-  lastNameInput.setAttribute("name", "last")
-  inputLast.appendChild(lastNameInput)
-
-  let errorLast = document.createElement("p")
-  errorLast.classList.add("errorMesage")
-  errorLast.setAttribute("id", "errorLast")
-  inputLast.appendChild(errorLast)
-
-  // email field
-
-  let inputEmail = document.createElement("div")
-  inputEmail.classList.add("formData")
-  inputEmail.setAttribute("id","input_email")
-  firstName.appendChild(inputEmail)
-
-  let emailFieldName = document.createElement("p")
-  emailFieldName.classList.add("input_name")
-  emailFieldName.innerHTML = "email"
-  inputEmail.appendChild(emailFieldName)
-
-  let emailLabel = document.createElement("label")
-  emailLabel.setAttribute("for" , "email")
-  inputEmail.appendChild(emailLabel)
-
-  let emailInput = document.createElement('input')
-  emailInput.classList.add("text-control")
-  emailInput.setAttribute("id" , "email")
-  emailInput.setAttribute("name" , "email")
-  emailInput.setAttribute("placeholder", "adresse@email.com")
-  inputEmail.appendChild(emailInput)
-
-  let emailError = document.createElement("p")
-  emailError.classList.add("errorMesage")
-  emailError.setAttribute("id" , "errorEmail")
-  inputEmail.appendChild(emailError)
-
-  //your mesage field
-
-  let inputMssg = document.createElement("div")
-  inputMssg.classList.add("formData")
-  inputMssg.setAttribute("id","input_message")
-  firstName.appendChild(inputMssg)
-
-  let mssgFieldName = document.createElement("p")
-  mssgFieldName.classList.add("input_name")
-  mssgFieldName.innerHTML = "Votre message"
-  inputMssg.appendChild(mssgFieldName)
-
-  let mssgLabel = document.createElement("label")
-  mssgLabel.setAttribute("for" , "message")
-  inputMssg.appendChild(mssgLabel)
-
-  let mssgInput = document.createElement('input')
-  mssgInput.classList.add("text-control")
-  mssgInput.setAttribute("type" ,"text")
-  mssgInput.setAttribute("id" , "form_message")
-  mssgInput.setAttribute("name" , "message")
-  mssgInput.setAttribute("aria-label", "Your message")
-  inputMssg.appendChild(mssgInput)
-
-  let mssgError = document.createElement("p")
-  mssgError.classList.add("errorMesage")
-  mssgError.setAttribute("id" , "errorMessage")
-  inputMssg.appendChild(mssglError)
-
+  
 
   //event listeners
   document.getElementById("first").addEventListener("change" , validateContact);
