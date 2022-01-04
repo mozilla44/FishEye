d7df2d5aa87d00bdc61e62412decb3e795edbc12
antoine <antoine.frisco@gmail.com>
2022-01-04 16:29:39 +0100

    fixed submit btn

--- scripts/contact_modal.js
+++ scripts/contact_modal.js
@@ -1,1 +1,5 @@
 /* validate form using validate API */
+document.querySelector('#contact_form input[type = "submit"]').addEventListener("click" ,(e)=>{
+  e.preventDefault();
+  let isValid = document.getElementById("contact_form").reportValidity(); 
+});
