7a04096bf8c554d20f4abf7b52bd2e6c635c7e6b
antoine <antoine.frisco@gmail.com>
2022-02-02 17:15:21 +0100

    hide scroll

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -10,6 +10,7 @@ class Diaporama {
     show(id) {
         this.current = this.listMedia.find((media) => media.id == id);
         document.querySelector("#diaporama").classList.add("show")
+        document.body.style.overflow='hidden'
         this.display();
     }
 
@@ -56,22 +57,31 @@ class Diaporama {
     prepareEvent() {
         document.querySelector(".close-lightbox").addEventListener("click", () => {
             document.querySelector("#diaporama").classList.remove("show");
+            document.body.style.overflow='auto';
         })
 
-        document.addEventListener('keydown', function (e) {
+        document.addEventListener('keydown', (e)=> {
             if (e.key == "Escape") {
                 document.querySelector("#diaporama").classList.remove("show");
+                document.body.style.overflow='auto'
+            }
+            if (e.key == "ArrowLeft") {
+                this.previous()
+            }
+            if (e.key == "ArrowRight") {
+                this.next()
             }
         });
 
+        //previous
         this.element.querySelector(".previous_lightbox").addEventListener("click", () => {
             this.previous();
         })
 
+        //next
         this.element.querySelector(".next_lightbox").addEventListener("click", () => {
             this.next();
         })
-
     }
 
     display() {
--- scripts/profiles.js
+++ scripts/profiles.js
@@ -2,7 +2,7 @@ let searchParam = (new URL(window.location).searchParams)
 
 let id = searchParam.get("id");
 console.log(id);
-
+let diapo = null
 
 
 fetch('../data.json')
@@ -21,9 +21,12 @@ fetch('../data.json')
     document.querySelector(".photographer_presentation").innerHTML = photographer.createPhotographerHeader()
     document.getElementById("contact_btn").addEventListener("click", (e) => {
       document.querySelector(".bground").classList.add("active");
+      document.getElementById("contact_btn").classList.add("hidden");
+
     })
     document.getElementById("close_modal_btn").addEventListener("click", (x) => {
-      document.querySelector(".bground").classList.remove("active")
+      document.querySelector(".bground").classList.remove("active");
+      document.getElementById("contact_btn").classList.remove("hidden");
     })
 
     let listMedia = [];
@@ -32,35 +35,70 @@ fetch('../data.json')
     findMedia.forEach(m => {
 
       let media = new MediaFactory(m, photographer.name);
-      listMedia.push(media)
-      document.querySelector(".photographer_gallery").innerHTML += media.createMedia();
-
-      document.querySelectorAll(".gallery_item .fa-heart").forEach(heart => {
-        heart.addEventListener('click', function (e) {
-          if (e.currentTarget.classList.contains('fa-heart')) {
-            let likes = e.currentTarget.closest(".preview_info").querySelector(".number_likes");
-            console.log(likes.innerHTML)
-            likes.innerHTML = parseInt(likes.innerHTML) + 1;
-            let totalLikes = document.querySelector("#bottom-likes")
-            totalLikes.innerHTML = parseInt(totalLikes.innerHTML) + 1;
-          }
-        });
-      })
+      listMedia.push(media);
 
     });
-    var diapo = new Diaporama(listMedia ,"#diaporama");
-      
-      document.querySelectorAll(".photographer_gallery .gallery_item").forEach(item => {
-        item.addEventListener("click" , (e) =>{
-          diapo.show(e.currentTarget.dataset.id)
+
+    displayMedia(listMedia)
+    diapo = new Diaporama(listMedia, "#diaporama");
+
+
+
+    //sort media
+    document.querySelector(".sorting select").addEventListener('change', (e) => {
+
+      if (e.target.value == "Popularité") {
+        listMedia.sort((a, b) => b.likes - a.likes)
+      }
+      if (e.target.value == "Date") {
+        listMedia.sort((a, b) => new Date(b.date) - new Date(a.date))
+      }
+      if (e.target.value == "Titre") {
+        listMedia.sort((a, b) => {
+          if (a.title < b.title) {
+            return -1;
+          }
+          if (a.title > b.title) { 
+            return 1;
+          }
         })
-      }) 
+      }
+      displayMedia(listMedia)
+
+    });
   })
+
+
   .catch(function (err) {
     console.log(err)
   })
 
 
+function displayMedia(listMedia) {
+
+  document.querySelector(".photographer_gallery").innerHTML = "";
+
+  for (let media of listMedia) {
+    document.querySelector(".photographer_gallery").innerHTML += media.createMedia();
+    document.querySelectorAll(".gallery_item .fa-heart").forEach(heart => {
+      heart.addEventListener('click', function (e) {
+        if (e.currentTarget.classList.contains('fa-heart')) {
+          let likes = e.currentTarget.closest(".preview_info").querySelector(".number_likes");
+          console.log(likes.innerHTML)
+          likes.innerHTML = parseInt(likes.innerHTML) + 1;
+          let totalLikes = document.querySelector("#bottom-likes")
+          totalLikes.innerHTML = parseInt(totalLikes.innerHTML) + 1;
+        }
+      });
+    })
+  }
+  document.querySelectorAll(".photographer_gallery .gallery_item img").forEach(item => {
+    item.addEventListener("click", (e) => {
+      diapo.show(e.currentTarget.closest(".gallery_item").dataset.id)
+    })
+  })
+
+}
 
 
 
